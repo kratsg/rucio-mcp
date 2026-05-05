6604d89fec91e851bd3dc55ee14ec5017d9606cf
Giordon Stark <kratsg@gmail.com>
2026-05-05 08:12:01 -0700

    pre-commit

--- src/rucio_mcp/__init__.py
+++ src/rucio_mcp/__init__.py
@@ -4,6 +4,7 @@ rucio-mcp: MCP Server for rucio
 """
 
 from __future__ import annotations
-from rucio_mcp._version import __version__
+
+from rucio_mcp._version import version as __version__
 
 __all__ = ["__version__"]
